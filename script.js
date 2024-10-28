d9ad8c18c0d5a31a6193096086cc0f997a5aa40b
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-10-28 16:38:58 +0100

    ADD esercizio

---
+++ script.js
@@ -0,0 +1,61 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png",
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png",
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png",
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png",
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png",
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png",
+  },
+];
+
+const cols = document.querySelectorAll(".col-4");
+
+teamMembers.forEach((member, index) => {
+  if (index < cols.length) {
+    cols[index].innerHTML = `
+      <div class="card mb-3 text-white bg-dark">
+        <div class="row">
+          <div class="col-4">
+            <img src="${member.img}" class="img-fluid rounded-start" alt="${member.name}" />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${member.name}</h5>
+              <p class="card-text">${member.role}</p>
+              <a href="#"><p class="card-text">${member.email}</p></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+});
